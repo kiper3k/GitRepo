ad8431d1a733c12206a6b8425e9e805bc94d0e11
kiperek <monikacabaj@vp.pl>
2014-01-22 14:53:53 +0100

    Create of equation - changed

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
+#include "add.h"
+#include "power.h"
+#include "multiply.h"
+#include <iostream>
+
 int main()
 {
+	std::cout << power(add(multiply(2,2),4),3)<<std::endl;
+	
 	return 0;
 }
---
+++ power.h
@@ -0,0 +1,10 @@
+double power(double a, unsigned int p)
+{
+	double result = 1;
+	
+	for(unsigned int i=0; i<p;i++)
+	{
+		result*=a;
+	}
+	return result;
+}
